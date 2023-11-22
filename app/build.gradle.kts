6ee09ff316ec9d2d75fdc590820e130ad0989d0a
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2023-11-23 01:11:44 +0700

    profile signup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,10 @@ dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+    //agar gambar jadi bulat
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
