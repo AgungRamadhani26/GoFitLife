e58a4d302fec9cc009c9a2296616d841b5a75b7a
iar98 <irvanalrasyid0@gmail.com>
2023-12-03 04:48:11 +0700

    aksi ketika button kembali perangkat di tekan muncul pesan, membuat fragment Drink page, menambahkan aksi keyboard di drink page

--- app/src/main/java/com/example/fitlife/BottomNavbarActivity.kt
+++ app/src/main/java/com/example/fitlife/BottomNavbarActivity.kt
@@ -2,7 +2,10 @@ package com.example.fitlife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.example.fitlife.Fragment.ClockFragment
 import com.example.fitlife.Fragment.DrinkFragment
@@ -12,8 +15,10 @@ import com.example.fitlife.Fragment.UserFragment
 import com.example.fitlife.databinding.ActivityBottomNavbarBinding
 
 class BottomNavbarActivity : AppCompatActivity() {
-
+    // variabel binding activity_bottom_navbar.xml
     private lateinit var binding: ActivityBottomNavbarBinding
+    // variable double clicked
+    private var doubleBackToExitPressedOnce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBottomNavbarBinding.inflate(layoutInflater)
@@ -31,6 +36,26 @@ class BottomNavbarActivity : AppCompatActivity() {
 
             return@setOnItemSelectedListener true
         }
+
+        // Aksi menangani tombol kembali
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (!doubleBackToExitPressedOnce) {
+                    doubleBackToExitPressedOnce = true
+                    // jika hanya menekan sekali akan muncul toast
+                    Toast.makeText(this@BottomNavbarActivity, "Tekan sekali lagi untuk keluar!!!", Toast.LENGTH_SHORT).show()
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        doubleBackToExitPressedOnce = false
+                    }, 2000)
+                } else {
+                    // Menutup semua aktivitas dan memulai kembali
+                    finishAffinity()
+                }
+            }
+        }
+
+        // menjalankan aksi function callback
+        onBackPressedDispatcher.addCallback(this, callback)
     }
 
     private fun setFragment(fragment: Fragment) {
---
+++ app/src/main/java/com/example/fitlife/models/Model4.kt
@@ -0,0 +1,8 @@
+package com.example.fitlife.models
+
+data class Model4(
+    val image: Int,
+    val judul: String,
+    val subJudul1: String,
+    val subJudul2: String
+)
