63dc7243c58f0bf35b45149ca005e814811192d1
silhquette <muhammadqiyam03@gmail.com>
2023-12-20 17:50:37 +0700

    loading animation article2

--- app/src/main/java/com/example/fitlife/adapters/ArticleAdapter.kt
+++ app/src/main/java/com/example/fitlife/adapters/ArticleAdapter.kt
@@ -3,8 +3,6 @@ package com.example.fitlife.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.example.fitlife.R
 import com.example.fitlife.databinding.CardArticleBinding
 import com.example.fitlife.models.News
 
